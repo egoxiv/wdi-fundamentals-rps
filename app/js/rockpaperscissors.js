9f4db10cb0c3a644203ed4ba96aab7c60bb45ade
Erik Gomez <egoxiv@gmail.com>
2016-02-06 14:49:18 -0800

    Added r,p,s logic to playToFive function. Logic tracks round, round winner, and current score. Displays winner and round number when someone reaches 5

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -81,8 +81,44 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var round = 0;
+
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+
+    while (playerWins < 5 && computerWins < 5) {
+        //var playerMove = randomPlay(); // for random play
+        var playerMove = getPlayerMove();
+        var computerMove = randomPlay();
+        var winner = getWinner(playerMove, computerMove);
+        if(winner == "tie") {
+            round++;
+            console.warn("Tie!")
+            console.log("player chose " + playerMove);
+            console.log("computer chose " + computerMove);
+        }
+        if (winner == "player") {
+            round++;
+            playerWins++;
+            console.log("Round " + round + " Player chose " + playerMove + " and computer chose " + computerMove +". Everyone knows that " + playerMove + " beats " + computerMove);
+            console.log("player score: " + playerWins);
+            console.log("computer score: " + computerWins);
+        }
+        if (winner == "computer") {
+            round++;
+            computerWins++;
+            console.log("Round " + round + " Computer chose " + computerMove + " and player chose " + playerMove +". Everyone knows that " + computerMove + " beats " + playerMove);
+            console.log("computer score: " + computerWins);
+            console.log("player score: " + playerWins);
+        }
+    }
+
+    if(playerWins === 5) {
+        console.warn("player wins on round " + round);
+    }
+    if(computerWins === 5) {
+        console.warn("computer wins on round " + round);   
+    }
+
     return [playerWins, computerWins];
 }
-
